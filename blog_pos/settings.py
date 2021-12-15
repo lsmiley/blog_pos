c7877ff69d91af9707d4d9db328cbec91abd0d27
lsmiley <lsmiley@us.ibm.com>
2021-12-15 12:18:02 -0500

    major
    major

--- blog_pos/settings.py
+++ blog_pos/settings.py
@@ -110,6 +110,8 @@ WSGI_APPLICATION = 'blog_pos.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+# **** Local Database Settings ****
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -120,6 +122,19 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+# **** Cloud Database Settings ****
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'esstools88db',
+#         'USER': 'sizingadmin',
+#         'PASSWORD': 'Malware12345',
+#         'HOST': '10.128.4.173',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
